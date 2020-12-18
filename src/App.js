75ed8cdec0df80258052aeedd2581faea7337ae3
PaulNguyen <giabaofrontend@gmail.com>
2020-12-19 00:14:36 +0700

    update api github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/layouts/NavBar';
-import Users from './components/users/Users';
-
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/layouts/NavBar";
+import Users from "./components/users/Users";
+import axios from "axios";
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <NavBar />
-        <div className='container'>
-          <Users />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );

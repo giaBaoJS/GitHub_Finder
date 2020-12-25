d4e448575b762b76c7a2c248a3c2180ceadc2069
paul <waycaosi1994@gmail.com>
2020-12-25 16:52:49 +0700

    fixed app, apply Context API

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,12 @@
-import React from "react";
-import Spinner from "../layouts/Spinner";
-import UserItem from "./UserItem";
-import PropTypes from "prop-types";
+import React, { useContext } from 'react';
+import Spinner from '../layouts/Spinner';
+import UserItem from './UserItem';
+import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
   if (loading) {
     return <Spinner />;
   } else {
@@ -17,14 +20,9 @@ const Users = ({ users, loading }) => {
   }
 };
 
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
-};
-
 const userStyle = {
-  display: "grid",
-  gridTemplateColumns: "repeat(3,1fr)",
-  gridGap: "1rem",
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGap: '1rem',
 };
 export default Users;
